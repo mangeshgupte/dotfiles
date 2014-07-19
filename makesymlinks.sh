1d1475391868cf68ce33dcebd6f9282b899881f8
Mangesh Gupte <mangesh@houzz.com>
2014-07-18 17:08:45 -0700

    Changing message

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -7,25 +7,28 @@
 ########## Variables
 
 dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
+olddir=~/.dotfiles.old             # old dotfiles backup directory
 files="zshrc oh-my-zsh"    # list of files/folders to symlink in homedir
 
 ##########
 
 # create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
+echo -n "Creating $olddir for backup of any existing dotfiles in ~ ... "
 mkdir -p $olddir
 echo "done"
 
 # change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
+echo -n "Changing to the $dir ... "
 cd $dir
 echo "done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+echo "Backing up and linking files"
 for file in $files; do
-	echo "Backup : mv ~/.$file ~/.dotfiles.old/"
-	mv ~/.$file ~/.dotfiles.old/
-	echo "Link   : ln -s $dir/$file ~/.$file"
-    ln -s $dir/$file ~/.$file
+	echo -n "$file ... "
+	mv ~/.$file $olddir
+	echo -n "moved ... "
+	ln -s $dir/$file ~/.$file
+	echo "linked"
 done
+echo "done"
