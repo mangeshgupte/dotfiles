47e0c7f5113131fa615ee4b7fbf77c2878d0a0cb
Mangesh Gupte <mangesh@meta.com>
2026-03-16 10:15:58 -0700

    Clean up switching.el: remove dead code and unnecessary require guards
    - Remove commented-out configs (find-file-at-point, ido-clever-match,
      ido-vertical-mode, smex)
    - Remove unnecessary require guards for built-in packages (recentf,
      uniquify, ibuffer)
    - Remove dead functions: ido-ignore-non-user-except-ielm (never hooked),
      ibuffer-ido-find-file (never bound or called)
    - Fix typo in comment (extentsions → extensions)

--- emacs.d/init/switching.el
+++ emacs.d/init/switching.el
@@ -7,13 +7,11 @@
   (setq session-use-package t)
   (add-hook 'after-init-hook 'session-initialize))
 
-(when (require 'recentf nil 'noerror)
-  (recentf-mode 1))
+(recentf-mode 1)
 
-(when (require 'uniquify nil 'noerror)
-  (setq
-   uniquify-buffer-name-style 'post-forward
-   uniquify-separator ":"))
+(setq
+ uniquify-buffer-name-style 'post-forward
+ uniquify-separator ":")
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -21,16 +19,9 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'ffap)
 (ffap-bindings)
-;; (global-unset-key "\C-x\C-f\C-f")
-;; (global-set-key "\C-x\C-f\C-f" 'find-file-at-point)
 (setq ffap-require-prefix t)
 
 ;; ido
-(defun ido-ignore-non-user-except-ielm (name)
-  "Ignore all non-user (a.k.a. *starred*) buffers except *ielm*."
-  (and (string-match "^\*" name)
-       (not (string= name "*ielm*"))))
-
 (when (require 'ido nil 'noerror)
   (ido-mode t)
   (setq ido-enable-flex-matching t)     ; fuzzy matching is a must have
@@ -95,48 +86,19 @@
   (flx-ido-mode 1)
   ;; disable ido faces to see flx highlights.
   (setq ido-use-faces nil))
-;; (when (require 'ido-clever-match nil 'noerror)
-;;   (ido-clever-match-enable))
 
 (when (require 'ido-grid-mode nil 'noerror)
   (ido-grid-mode t))
-;; (when (require 'ido-vertical-mode nil 'noerror)
-;;   (ido-vertical-mode t))
 
-;; (when (require 'smex nil 'noerror)
-;;   (global-set-key (kbd "M-x") 'smex)
-;;   (global-set-key (kbd "M-X") 'smex-major-mode-commands)
-;;   ;; This is your old M-x.
-;;   (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command))
-
-(when (require 'ibuffer nil 'noerror)
-  (global-set-key (kbd "C-x C-b") 'ibuffer))
-
-(defun ibuffer-ido-find-file (file &optional wildcards)
-  "Like `ido-find-file', but default to the directory of the buffer at point."
-  (interactive
-   (let ((default-directory
-           (let ((buf (ibuffer-current-buffer)))
-             (if (buffer-live-p buf)
-                 (with-current-buffer buf
-                   default-directory)
-               default-directory))))
-     (list (ido-read-file-name "Find file: " default-directory) t)))
-  (find-file file wildcards))
+(global-set-key (kbd "C-x C-b") 'ibuffer)
 
 ;; Good for opening header file under cursor
 (global-set-key "\C-cf" 'open-file-under-cursor)
 (fset 'open-file-under-cursor
    [?\C-\M-b ?\C-  ?\C-\M-f ?\C-\M-f ?\M-w ?\C-x ?\C-f ?\C-y return])
 
-;; ignored extentsions
+;; ignored extensions
 (delete ".log" completion-ignored-extensions)
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Custom built functions
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 
 (global-set-key (kbd "C-x f") 'find-file-in-repository)
 (global-set-key [C-double-mouse-1] 'browse-url)
